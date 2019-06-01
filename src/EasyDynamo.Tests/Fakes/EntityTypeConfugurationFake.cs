d840f32442db02afa860ffbf48cd710fedd4dc8e
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-06-02 00:44:36 +0300

    Added some fakes.

---
+++ src/EasyDynamo.Tests/Fakes/EntityTypeConfugurationFake.cs
@@ -0,0 +1,18 @@
+﻿using EasyDynamo.Abstractions;
+using System;
+
+namespace EasyDynamo.Tests.Fakes
+{
+    public class EntityTypeConfugurationFake : IEntityTypeConfiguration<FakeEntity>
+    {
+        public bool ConfigureInvoked { get; private set; }
+
+        public Type ConfigureInvokedWithBuilderType { get; private set; }
+
+        public void Configure(IEntityTypeBuilder<FakeEntity> builder)
+        {
+            this.ConfigureInvoked = true;
+            this.ConfigureInvokedWithBuilderType = builder.GetType();
+        }
+    }
+}
---
+++ src/EasyDynamo.Tests/Fakes/ModelBuilderFake.cs
@@ -0,0 +1,13 @@
+﻿using EasyDynamo.Abstractions;
+using EasyDynamo.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace EasyDynamo.Tests.Fakes
+{
+    public class ModelBuilderFake : ModelBuilder
+    {
+        public IDictionary<Type, IEntityConfiguration> EntityConfigurationByEntityTypesFromBase
+            => base.EntityConfigurationByEntityTypes;
+    }
+}
