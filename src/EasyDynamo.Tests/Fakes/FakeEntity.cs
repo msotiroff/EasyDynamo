418d9f488e0c12c8467ca5bcb951c2862375450a
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-06-02 00:05:23 +0300

    Added some properties.

--- src/EasyDynamo.Tests/Fakes/FakeEntity.cs
+++ src/EasyDynamo.Tests/Fakes/FakeEntity.cs
@@ -1,7 +1,17 @@
-﻿namespace EasyDynamo.Tests.Fakes
+﻿using System;
+
+namespace EasyDynamo.Tests.Fakes
 {
     public class FakeEntity
     {
         public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public string IgnoreMe { get; set; }
     }
 }
