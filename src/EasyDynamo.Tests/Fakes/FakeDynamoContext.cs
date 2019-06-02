b784b0edc1160ed231e931144726b4e2e1d5a798
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-06-02 20:03:37 +0300

    Added fake inheritor of DynamoContext.

---
+++ src/EasyDynamo.Tests/Fakes/FakeDynamoContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EasyDynamo.Builders;
+using EasyDynamo.Core;
+using Microsoft.Extensions.Configuration;
+
+namespace EasyDynamo.Tests.Fakes
+{
+    public class FakeDynamoContext : DynamoContext
+    {
+        public FakeDynamoContext(IServiceProvider serviceProvider) 
+            : base(serviceProvider)
+        {
+        }
+
+        public static bool OnConfiguringInvoked { get; set; }
+
+        public static bool OnModelCreatingInvoked { get; set; }
+
+        protected override void OnConfiguring(
+            DynamoContextOptionsBuilder builder, IConfiguration configuration)
+        {
+            OnConfiguringInvoked = true;
+
+            base.OnConfiguring(builder, configuration);
+        }
+
+        protected override void OnModelCreating(
+            ModelBuilder builder, IConfiguration configuration)
+        {
+            OnModelCreatingInvoked = true;
+
+            base.OnModelCreating(builder, configuration);
+        }
+    }
+}
