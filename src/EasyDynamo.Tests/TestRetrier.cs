346e2c6de85ab276e16e9123357e25e6253ed644
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-06-02 20:02:16 +0300

    Added TestRetrier.

---
+++ src/EasyDynamo.Tests/TestRetrier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EasyDynamo.Tests
+{
+    public static class TestRetrier
+    {
+        public static async Task RetryAsync(Action action, int retries = 5, int delay = 100)
+        {
+            var passed = false;
+
+            while (!passed && retries > 0)
+            {
+                try
+                {
+                    action();
+
+                    passed = true;
+                }
+                catch
+                {
+                    retries--;
+
+                    await Task.Delay(delay);
+                }
+            }
+        }
+    }
+}
