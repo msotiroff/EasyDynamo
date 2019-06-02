83c66e4d462acc93a6b892472a18231ab381a3aa
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-06-02 21:31:42 +0300

    Added unit tests for TableDropper.

---
+++ src/EasyDynamo.Tests/Tools/TableDropperTests.cs
@@ -0,0 +1,77 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using EasyDynamo.Exceptions;
+using EasyDynamo.Tools;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyDynamo.Tests.Tools
+{
+    public class TableDropperTests
+    {
+        private readonly Mock<IAmazonDynamoDB> clientMock;
+        private readonly TableDropper dropper;
+
+        public TableDropperTests()
+        {
+            this.clientMock = new Mock<IAmazonDynamoDB>();
+            this.dropper = new TableDropper(this.clientMock.Object);
+        }
+
+        [Fact]
+        public async Task DropTableAsync_CallsClientWithCorrectTableName()
+        {
+            const string TableName = "Articles";
+
+            this.clientMock
+                .Setup(cli => cli.DeleteTableAsync(
+                    It.Is<DeleteTableRequest>(dtr => dtr.TableName == TableName),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new DeleteTableResponse
+                {
+                    HttpStatusCode = System.Net.HttpStatusCode.OK
+                })
+                .Verifiable();
+
+            await this.dropper.DropTableAsync(TableName);
+
+            this.clientMock.Verify();
+        }
+
+        [Fact]
+        public async Task DropTableAsync_ResponseFail_ThrowsException()
+        {
+            const string TableName = "Articles";
+
+            this.clientMock
+                .Setup(cli => cli.DeleteTableAsync(
+                    It.IsAny<DeleteTableRequest>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new DeleteTableResponse
+                {
+                    HttpStatusCode = System.Net.HttpStatusCode.BadRequest
+                });
+
+            await Assert.ThrowsAsync<DeleteTableFailedException>(
+                () => this.dropper.DropTableAsync(TableName));
+        }
+
+        [Fact]
+        public async Task DropTableAsync_ClientThrows_ThrowsException()
+        {
+            const string TableName = "Articles";
+
+            this.clientMock
+                .Setup(cli => cli.DeleteTableAsync(
+                    It.IsAny<DeleteTableRequest>(),
+                    It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new InvalidOperationException());
+
+            await Assert.ThrowsAsync<DeleteTableFailedException>(
+                () => this.dropper.DropTableAsync(TableName));
+        }
+    }
+}
