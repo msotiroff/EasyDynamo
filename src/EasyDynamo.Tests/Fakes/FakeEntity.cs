947788e0c6f2e0291b537c837f159da8b021116f
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-06-02 20:56:40 +0300

    Added attributes to test IndexConfigurationFactory.

--- src/EasyDynamo.Tests/Fakes/FakeEntity.cs
+++ src/EasyDynamo.Tests/Fakes/FakeEntity.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
 
 namespace EasyDynamo.Tests.Fakes
 {
     public class FakeEntity
     {
+        public const string IndexName = "GSI_FakeEntity_Title_LastModified";
+
         public int Id { get; set; }
 
+        [DynamoDBGlobalSecondaryIndexHashKey(IndexName)]
         public string Title { get; set; }
 
         public string Content { get; set; }
 
+        [DynamoDBGlobalSecondaryIndexRangeKey(IndexName)]
         public DateTime LastModified { get; set; }
 
         public string IgnoreMe { get; set; }
