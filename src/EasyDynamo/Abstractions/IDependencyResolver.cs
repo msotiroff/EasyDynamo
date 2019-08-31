f30f72d42edf9958525cee21f18aef23275dd34b
Mihail Sotirov <mihail.sotiroff@gmail.com>
2019-09-01 00:26:20 +0300

    Added an abstraction for the IoC container.

---
+++ src/EasyDynamo/Abstractions/IDependencyResolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EasyDynamo.Abstractions
+{
+    public interface IDependencyResolver
+    {
+        TDependency GetDependency<TDependency>();
+
+        object GetDependency(Type dependencyType);
+    }
+}
---
+++ src/EasyDynamo/Tools/Resolvers/ServiceProviderDependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using EasyDynamo.Abstractions;
+using System;
+
+namespace EasyDynamo.Tools.Resolvers
+{
+    public class ServiceProviderDependencyResolver : IDependencyResolver
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ServiceProviderDependencyResolver(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public TDependency GetDependency<TDependency>()
+        {
+            return (TDependency)this.GetDependency(typeof(TDependency));
+        }
+
+        public object GetDependency(Type dependencyType)
+        {
+            return this.serviceProvider.GetService(dependencyType);
+        }
+    }
+}
